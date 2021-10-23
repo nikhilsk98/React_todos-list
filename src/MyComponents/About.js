d549ceb2b9c3fc2f05ba1772988a58c718616810
Nikhil <nikhilsk98@gmail.com>
2021-10-24 00:08:29 +0530

    init

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere laborum sapiente, fugiat porro consequuntur sunt rerum repellat unde quas fuga necessitatibus! Eveniet dolores in molestiae corrupti architecto unde ut. Ab?</p>
+        </div>
+    )
+}
